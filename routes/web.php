1069fdf78ce5d30424e4af3e1dff5492220de603
Alvin Bintang Rebrastya <alvinrr91@gmail.com>
2025-08-29 14:28:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OcrController;
 
-Route::get('/ocr', [OcrController::class, 'index']);
+Route::get('/ocr', [OcrController::class, 'index'])->name('ocr.index');
 Route::post('/ocr', [OcrController::class, 'extract'])->name('ocr.extract');
 Route::get('/ocr/result/{id}', [OcrController::class, 'showResult'])->name('ocr.result'); // ADDED
